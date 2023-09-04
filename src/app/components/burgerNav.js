c64493a2a8906e847459d8a65c5f9f310fd6c579
isucic <isucic00@fesb.hr>
2023-09-04 16:36:51 +0200

    responsive navbar

--- src/app/components/burgerNav.js
+++ src/app/components/burgerNav.js
@@ -2,10 +2,24 @@
 
 import { useState } from "react";
 import BurgerNavIcon from "./burgerNavIcon";
+import {usePathname} from "next/navigation"
+import Link from "next/link";
 
 
 const BurgerNav = () => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const menuItems = [
+        {title: "reservation", link: ""},
+        {title: "car fleet", link: "fleet"},
+        {title: "news", link: "news"},
+        {title: "about us", link: "aboutus"},
+    ]
+
+    const pathname = usePathname()
+    const isActive = (ime) => {
+         return pathname.split("/")[1] === ime.split("/")[1]
+    }
 
     return(
         <div className="md:hidden flex flex-col">
@@ -15,8 +29,27 @@ const BurgerNav = () => {
 
             <main className={`${
                 !isOpen ? 'translate-x-full' : 'translate-x-0'
-            } transform translate fixed right-0 m-0 top-0 p-0 bg-fitify-black md:hidden transition duration-500 ease-in-out flex-col justify-center items-center h-screen z-20`}>
+            } transform translate fixed right-0 m-0 top-0 p-0 bg-rent-light-red md:hidden transition duration-500 ease-in-out flex-col justify-center items-center h-screen z-20`}>
                 <BurgerNavIcon clicked={isOpen} setIsClicked={setIsOpen} classes={" bg-red-800"} />
+                <div className="flex flex-col text-center mt-10">
+                    {menuItems.map((item) => {
+                            const itemLink = `/${item.link === "reservation" ? "" : item.link}`;
+                            return (
+                            <Link href={itemLink} key={item.title}>
+                                <p
+                                 className={`text-white text-lg font-comfortea py-4 px-8 whitespace-nowrap uppercase hover:bg-fitify-green
+                                 ${
+                                   isActive(itemLink)
+                                     ? "underline-offset-4 text-decoration-line: underline font-bold"
+                                     : ""
+                                 }`}
+                                >
+                             {item.title}{" "}
+                                </p>
+                            </Link>
+                        )
+                    })}
+                </div>
             </main>
         </div>
     )
