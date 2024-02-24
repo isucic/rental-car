4e4b6fc5b008acbb32230b746f6dfd47d60b3138
isucic <isucic00@fesb.hr>
2024-02-24 22:53:33 +0100

    change onhohover blog

--- src/app/components/blogCard.jsx
+++ src/app/components/blogCard.jsx
@@ -4,16 +4,21 @@ import Link from "next/link"
 
 const BlogCard = ({blog}) => {
     return (
+        <>
+        
+        <div className="flex flex-col justify-self-center mb-6 m-0 lg:m-2 ">
         <Link href={`news/${blog.id}`}>
-        <div className="flex flex-col justify-self-center mb-6 m-0 lg:m-2 md:hover:scale-105">
-            <div className="md:w-80 sm:w-80 w-64 md:h-64 sm:h-72 h-64 relative ">
+            <div className="md:w-80 sm:w-80 w-64 md:h-64 sm:h-72 h-64 relative overflow-hidden">
+            <div className="w-full h-full transform origin-center transition-transform duration-300 ease-in-out hover:scale-105">
                 <Image
                     src={blog.photo}
                     alt={blog.title}
                     fill
                     style={{objectFit:"cover"}}
                 />
+                </div>
             </div>
+            </Link>
 
             <div 
              className="font-comfortea relative xl:w-96 w-80 md:w-80 xl:h-52 h-56 md:h-56 p-5 rounded-md bg-rent-white
@@ -23,12 +28,14 @@ const BlogCard = ({blog}) => {
                 <h3 className="font-bold capitalize text-xl">{blog.title}</h3>
                 <p className="text-base text-rent-dark-grey my-4">{blog.article.substring(0,76) + '...'}</p>
 
-                <p className="w-fit redbtn text-base bg-rent-red text-rent-white p-2 rounded-md uppercase">more</p>
+                <Link href={`news/${blog.id}`}>
+                    <p className="w-fit redbtn text-base bg-rent-red text-rent-white p-2 rounded-md uppercase">more</p>
+                </Link>
             </div>
         </div>
-        </Link>
+      
+        </>
     )
 }
 
 export default BlogCard
-
